936d2f06f7d10f20735e1902f978657f61544235
Hassan <hassan_bht@yahoo.com>
2023-02-13 10:16:18 +0330

    impl SecureCredentialsStorage

---
+++ lib/auth/infrastructure/credentials_storage/secure_credentials_storage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:github_repo_app/auth/infrastructure/credentials_storage/credentials_storage.dart';
+import 'package:oauth2/src/credentials.dart';
+
+class SecureCredentialsStorage implements CredentialsStorage {
+  final FlutterSecureStorage _storage;
+
+  SecureCredentialsStorage(this._storage);
+
+  static const String _key = 'oauth2_credentials';
+  Credentials? _cachedCredentials;
+  @override
+  Future<Credentials?> read() async {
+    if (_cachedCredentials != null) return _cachedCredentials;
+    final json = await _storage.read(key: _key);
+    if (json == null) return null;
+    try {
+      return _cachedCredentials = Credentials.fromJson(json);
+    } on FormatException {
+      return null;
+    }
+  }
+
+  @override
+  Future<void> save(Credentials credentials) {
+    _cachedCredentials = credentials;
+    return _storage.write(key: _key, value: credentials.toJson());
+  }
+
+  @override
+  Future<void> clear() {
+    _cachedCredentials = null;
+    return _storage.delete(key: _key);
+  }
+}
---
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/services.dart';
+import 'package:github_repo_app/auth/infrastructure/credentials_storage/credentials_storage.dart';
+import 'package:oauth2/oauth2.dart';
+
+class GithubAuthenticator {
+  final CredentialsStorage _credentialsStorage;
+
+  GithubAuthenticator(this._credentialsStorage);
+
+  Future<Credentials?> getSignedInCredential() async {
+    try {
+      final storedCredentials = await _credentialsStorage.read();
+      if (storedCredentials != null) {
+        if (storedCredentials.canRefresh && storedCredentials.isExpired) {
+          //TODO :Refresh
+        }
+      }
+      return storedCredentials;
+    } on PlatformException {
+      return null;
+    }
+  }
+
+  Future<bool> isSignedIn() =>
+      getSignedInCredential().then((credentials) => credentials != null);
+}
