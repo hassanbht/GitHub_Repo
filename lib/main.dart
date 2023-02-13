909865d59f7c5ed05686710980d7a84b150095b0
Hassan <hassan_bht@yahoo.com>
2023-02-13 10:15:54 +0330

    impl SecureCredentialsStorage

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: const Text('Material App Bar'),
         ),
         body: Center(
           child: Container(
